effe5b7c51acefcf5b1630d132e818096163fc6c
Matty Gabbitas <mattytv11@gmail.com>
2015-05-05 16:43:59 +0100

    rate

---
+++ app/models/movie.rb
@@ -0,0 +1,10 @@
+class Movie < ActiveRecord::Base
+	ratyrate_rateable 'visual_effects', 'original_score', 'director', 'custome_design'
+  def poster
+    "http://ia.media-imdb.com/images/M/#{poster_url}"
+  end
+ 
+  def imdb
+    "http://www.imdb.com/title/#{imdb_id}/"
+  end
+end
